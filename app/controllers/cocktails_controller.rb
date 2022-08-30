164fcf3e009ad63c1cae213e3bae4e1dff77dce4
adrienne robert vassy <adrienne.robertvassy@gmail.com>
2022-08-30 17:42:50 +0200

    we got a cocktails index and cocktail show yeeehaaaw

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,9 +1,11 @@
 class CocktailsController < ApplicationController
   def index
     @cocktails = Cocktail.all
+    @ingredients = Ingredient.all
   end
 
   def show
-    @cocktail = Cocktail.find_by(params[:id])
+    @cocktail = Cocktail.find(params[:id])
+    @ingredients = Ingredient.all
   end
 end
