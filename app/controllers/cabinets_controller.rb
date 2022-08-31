0cfa8c0c8058b7e30e3400893efe22bf58fef310
Onofre Catala Castellar <onofrecc@gmail.com>
2022-08-31 14:49:15 +0200

    cabinet ui

--- app/controllers/cabinets_controller.rb
+++ app/controllers/cabinets_controller.rb
@@ -2,6 +2,10 @@ class CabinetsController < ApplicationController
   def create
   end
 
+  # def index
+  #   @user_ingredients = Cabinet.where(user == current_user)
+  # end
+
   def update
   end
 end
