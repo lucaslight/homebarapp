33996378c678aff4a59105504aacaa7d85b95ce5
Lucas Luz <lucas@calazansluz.com>
2022-08-31 17:51:55 +0200

    home and devise login/ou fix

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   get 'saved_cocktails/create'
   get 'saved_cocktails/index'
 
-  devise_for :users
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
