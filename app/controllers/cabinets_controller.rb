26956a2c5ff1637e7bef7e3a9e5cbb255acd90e8
Onofre Catala Castellar <onofrecc@gmail.com>
2022-08-31 17:38:43 +0200

    Ingredients in stock, cabinet of the user

--- app/controllers/cabinets_controller.rb
+++ app/controllers/cabinets_controller.rb
@@ -2,10 +2,6 @@ class CabinetsController < ApplicationController
   def create
   end
 
-  # def index
-  #   @user_ingredients = Cabinet.where(user == current_user)
-  # end
-
   def update
   end
 end
