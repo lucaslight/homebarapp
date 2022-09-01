1669a310d18a740bb4a45a6ba06c309e90d1488c
Onofre Catala Castellar <onofrecc@gmail.com>
2022-09-01 14:28:29 +0200

    small feature for stock or not in stock

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,17 @@ Rails.application.routes.draw do
   resources :cabinets, only: [:create, :update]
   resources :saved_cocktails, only: [:create, :index]
   resources :cocktails, only: [:show, :index]
-  
+
   devise_for :users do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  
+
   # get '/uikit', to: 'pages#uikit'
   # get 'cabinets/create'
   # get 'cabinets/update'
   # get 'saved_cocktails/create'
   # get 'saved_cocktails/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  patch "cabinets/:id/changes", to: "cabinets#remove_from_stock", as: "change"
 end
