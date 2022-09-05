ace907b488a6a0360573c4ba754faa4f15f1036e
Lucas Luz <lucas@calazansluz.com>
2022-09-05 16:52:24 +0200

    search comparation done but now being displayed yet

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,226 +1,3 @@
-# .-. .-. .-. .-. .-. . . .-. .
-# | | |(   |  |..  |  |\| |-| |
-# `-' ' ' `-' `-' `-' ' ` ` ' `-'
-
-# class CocktailsController < ApplicationController
-
-#   def show
-#     @cocktail = Cocktail.find(params[:id])
-#     @ingredients = Ingredient.all
-#   end
-
-#   def index
-#     @cocktails = Cocktail.all
-#     @ingredients = Ingredient.all
-
-#     # <p>TOP OF THE PAGE <%= @all_found_cocktails </p>
-
-#     if params[:search_query] && current_user
-
-#     #  CONVERT USER SEARCH INTO INGR INSTANCE
-#       search_ingr = Ingredient.find_by(name: params[:search_query])
-
-#     # CABINET LOGIC WITH USER SEARCH
-#       idk = Cabinet.find_by(ingredient: search_ingr, user: current_user)
-
-#       if idk && idk.in_stock == false
-#         idk.in_stock = true
-#         idk.save!
-#       elsif idk == nil
-#         Cabinet.create!(ingredient: search_ingr, user: current_user)
-#       end
-#     end
-
-    #  SEARCH FUNCTION BELOW
-    # found_cocktails_id = []
-    # array = Measurement.where(ingredient: search_ingr)
-    # array.each do |item|
-    #   found_cocktail_id = item.cocktail_id
-    #   found_cocktails_id << found_cocktail_id
-    # end
-
-    # @all_found_cocktails = []
-    # found_cocktails_id.each do |id|
-    #   cocktail = Cocktail.find_by(id:id)
-    #   @all_found_cocktails << cocktail
-    # end
-
-#     @user_ingredients_in_stock = Cabinet.where(user: current_user, in_stock: true).to_a
-#   end
-# end
-
-# .-. .-. .-. .-. .-. . . .-. .
-# | | |(   |  |..  |  |\| |-| |
-# `-' ' ' `-' `-' `-' ' ` ` ' `-'
-
-
-
-
-# class CocktailsController < ApplicationController
-
-#   def show
-#     @cocktail = Cocktail.find(params[:id])
-#     @ingredients = Ingredient.all
-#   end
-
-#   def index
-#     @cocktails = Cocktail.all
-#     @ingredients = Ingredient.all
-
-#     # <p>TOP OF THE PAGE <%= @all_found_cocktails </p>
-
-#     if params[:search_query] && current_user
-
-#       #  CONVERT USER SEARCH INTO INGR INSTANCE
-#         search_ingr = []
-#         search_ingr << Ingredient.find_by(name: params[:search_query])
-
-#       # CABINET LOGIC WITH USER SEARCH
-#         search_ingr.each do |ingredient|
-
-#           idk = Cabinet.find_by(ingredient: ingredient, user: current_user)
-#           if idk && idk.in_stock == false
-#             idk.in_stock = true
-#             idk.save!
-#           elsif idk == nil
-#             Cabinet.create!(ingredient: ingredient, user: current_user)
-#           end
-#         end
-#     end
-
-#     #  SEARCH FUNCTION BELOW
-#     found_cocktails_id = []
-#     array = []
-#     unless search_ingr.nil?
-
-#       search_ingr.each do |ingredient|
-#         array << Measurement.where(ingredient: ingredient)
-#       end
-
-#       array.each do |item|
-#         found_cocktail_id = item.find(item.cocktail_id)
-#         found_cocktails_id << found_cocktail_id
-#       end
-
-#       @all_found_cocktails = []
-#       found_cocktails_id.each do |id|
-#         cocktail = Cocktail.find_by(id)
-#         @all_found_cocktails << cocktail
-#       end
-
-#       cocktail_duplicates = []
-#       ready_cocktails = []
-#       one_cocktails = []
-#       two_cocktails = []
-#       cocktails.each do |cocktail|
-#         cocktail_duplicates << @all_found_cocktails.where(cocktail)
-#         case (cocktail_duplicates.count - Ingredient.where(cocktail).count).to_i
-#         when 0
-#           ready_cocktails << cocktail
-#         when -1
-#           one_cocktails << cocktail
-#         when -2
-#           two_cocktails << cocktail
-#         end
-#       end
-
-#     @user_ingredients_in_stock = Cabinet.where(user: current_user, in_stock: true).to_a
-#   end
-# end
-# end
-
-
-
-
-# class CocktailsController < ApplicationController
-
-#   def show
-#     @cocktail = Cocktail.find(params[:id])
-#     @ingredients = Ingredient.all
-#   end
-
-#   def index
-#     @cocktails = Cocktail.all
-#     @ingredients = Ingredient.all
-#     @user_ingredients_in_stock = Cabinet.where(user: current_user, in_stock: true).to_a
-
-#     # <p>TOP OF THE PAGE <%= @all_found_cocktails </p>
-
-#     if params[:search_query] && current_user
-
-#       #  CONVERT USER SEARCH INTO INGR INSTANCE
-#         search_ingr = []
-#         search_ingr << Ingredient.find_by(name: params[:search_query])
-
-#       # CABINET LOGIC WITH USER SEARCH
-#         idk = Cabinet.find_by(ingredient: search_ingr, user: current_user)
-
-#         if idk && idk.in_stock == false
-#           idk.in_stock = true
-#           idk.save!
-#         elsif idk == nil
-#           Cabinet.create!(ingredient: search_ingr, user: current_user)
-#         end
-#     end
-
-#     #  SEARCH FUNCTION BELOW
-#     found_cocktails_id = []
-#     array = [Ingredient.last]
-#     @user_ingredients_in_stock.each do |tag|
-#       array << Measurement.where(ingredient: tag)
-#     puts (array)
-#     end
-#     array.each do |item|
-#       found_cocktail_id = item.cocktail_id
-#       found_cocktails_id << found_cocktail_id
-#     end
-
-#     @all_found_cocktails = []
-#     found_cocktails_id.each do |id|
-#       cocktail = Cocktail.find_by(id)
-#       @all_found_cocktails << cocktail
-#     end
-
-#     #comparing
-#     @ready_cocktails = []
-#     @one_cocktails = []
-#     @two_cocktails = []
-#     @cocktails.each do |cocktail|
-#       cocktail_duplicates << @all_found_cocktails.where(cocktail)
-#       case (cocktail_duplicates.count - Ingredient.where(cocktail).count).to_i
-#       when 0
-#         @ready_cocktails << cocktail
-#       when -1
-#         @one_cocktails << cocktail
-#       when -2
-#         @two_cocktails << cocktail
-#       end
-#     end
-
-
-
-#   end
-# end
-
-# 1 - get input
-  #  tag
-# 2 - store tags
-# 3 - iterate thru cocktail to find cocktails that contain tag
-  # store each cocktail instance that contains each tag (meaning we have duplicates)
-# 4 - iterate thru results and count recurrence of each cocktail
-  # compare cocktail count to number of ingredients
-    #cocktail_count - cocktail_ingredient_count = num
-  # store cocktail in separate arrays if diff (num) "0 or 1 or 2"
-
-
-
-
-
-
-
-# .-. .-. .-. .-. .-. . .
-# `-. |-  |-| |(  |   |-|
-# `-' `-' ` ' ' ' `-' ' `
 class CocktailsController < ApplicationController
 
   def show
@@ -229,23 +6,48 @@ class CocktailsController < ApplicationController
   end
 
   def index
+
+    # .-. .-. .-. .-. .-. . .
+    # `-. |-  |-| |(  |   |-|
+    # `-' `-' ` ' ' ' `-' ' `
     @cocktails = Cocktail.all
     @ingredients = Ingredient.all
     @measurements = Measurement.all
-    @ingredients_my = Cabinet.where(user_id: current_user.id, in_stock: true)
-
-    # @ingredients = Ingredient.where(id: @tags).pluck(:name)
+    @user_ingredients_in_stock = Cabinet.where(user_id: current_user.id, in_stock: true)
+    @found_cocktails_id = []
+    @ready = []
+    @missing_one = []
+    @missing_two = []
 
     # 1 - get input
+    create_ingredient_tags
+    # 3 - Search based on (in stock) cabinet || @user_ingredients_in_stock
+    search_based_on_ingredient_tags
+    # 4 - Filter results to unique recurrence
+    @all_found_cocktails = Cocktail.where(id: @found_cocktails_id.uniq)
+    # 5 - Compare
+    @all_found_cocktails.pluck(:id).each do |cocktailid| #=> [id, id, id]
+      @result_cocktail_ingredients = Measurement.where(cocktail_id: cocktailid).pluck(:ingredient_id) #=> [id, id, id]
+      @result_cabinet_ingredients = @user_ingredients_in_stock.pluck(:ingredient_id) #=> [id, id, id]
+      @missing_ingredients = @result_cocktail_ingredients - @result_cabinet_ingredients
+      case @missing_ingredients.count
+      when 0
+        @ready = Cocktail.where(id: cocktailid)
+      when 1
+        @missing_one = Cocktail.where(id: cocktailid)
+      when 2
+        @missing_two = Cocktail.where(id: cocktailid)
+      end
+    end
+
+    # raise
+  end
+
+  def create_ingredient_tags
     if params[:search_query].present? && current_user
-      #  1.1 -tag
-      #  CONVERT USER SEARCH INTO INGR INSTANCE
       search_ingr = Ingredient.find_by(name: params[:search_query])
-
-      # 2 - store tags
-      # CABINET LOGIC WITH USER SEARCH
+      # 2 - save to cabinet || @user_ingredients_in_stock
       @idk = Cabinet.find_by(ingredient: search_ingr, user: current_user)
-
       if @idk && @idk.in_stock == false
         @idk.in_stock = true
         @idk.save!
@@ -253,21 +55,16 @@ class CocktailsController < ApplicationController
         Cabinet.create!(ingredient: search_ingr, user: current_user)
       end
     end
-    # 3 - iterate thru cocktail to find cocktails that contain tag
-    # store each cocktail instance that contains each tag (meaning we have duplicates)
-    @found_cocktails_id = []
+  end
 
-    @ingredients_my.pluck(:ingredient_id).each do |ingredient|
+  def search_based_on_ingredient_tags
+    @user_ingredients_in_stock.pluck(:ingredient_id).each do |ingredient|
       array = Measurement.where(ingredient_id: ingredient)
       array.each do |item|
         found_cocktail_id = item.cocktail_id
         @found_cocktails_id << found_cocktail_id
       end
     end
-
-    @all_found_cocktails = Cocktail.where(id: @found_cocktails_id.uniq)
-
-    @user_ingredients_in_stock = @ingredients_my
   end
 
     # 4 - iterate thru results and count recurrence of each cocktail
