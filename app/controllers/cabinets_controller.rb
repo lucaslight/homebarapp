d88ffe071b9aca0d98b3bd4e9aeff7cd89bc5531
Onofre Catala Castellar <onofrecc@gmail.com>
2022-09-06 19:08:30 +0200

    enough today

--- app/controllers/cabinets_controller.rb
+++ app/controllers/cabinets_controller.rb
@@ -6,10 +6,23 @@ class CabinetsController < ApplicationController
   end
 
   def add_to_stock
-    @cabinet = Cabinet.find(params[:id])
-    @cabinet.in_stock = true
-    @cabinet.save!
-    redirect_back(fallback_location: root_path)
+    @grocery_list.each do |item|
+      #  item
+      #  item.name
+      #  item.id
+      @item_cabinet = Cabinet.find_by(user_id: current_user.id, ingredient_id: item.id)
+      # @item_cabinet
+      if @item_cabinet && @item_cabinet.in_stock == false
+        @item_cabinet.in_stock = true
+        @item_cabinet.save!
+      elsif @item_cabinet == nil
+        Cabinet.create!(user_id: current_user.id, ingredient_id: item.id)
+      end
+      # @cabinet = Cabinet.find(params[:id])
+      # @cabinet.in_stock = true
+      # @cabinet.save!
+      redirect_back(fallback_location: root_path)
+    end
   end
 
   def remove_from_stock
