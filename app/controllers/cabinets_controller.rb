74e405e5e841e054650ff0581d869afacb4d4e9c
Onofre Catala Castellar <onofrecc@gmail.com>
2022-09-06 18:12:45 +0200

    working

--- app/controllers/cabinets_controller.rb
+++ app/controllers/cabinets_controller.rb
@@ -5,6 +5,13 @@ class CabinetsController < ApplicationController
   def update
   end
 
+  def add_to_stock
+    @cabinet = Cabinet.find(params[:id])
+    @cabinet.in_stock = true
+    @cabinet.save!
+    redirect_back(fallback_location: root_path)
+  end
+
   def remove_from_stock
     @cabinet = Cabinet.find(params[:id])
     @cabinet.in_stock = !@cabinet.in_stock
