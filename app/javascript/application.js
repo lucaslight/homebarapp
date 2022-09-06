adf31da0a313d6c965391673039903e7835941cd
Alvaro Trujillo <alvaro.trujillo@me.com>
2022-09-06 14:50:42 +0200

    Modal now interpolating

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,48 +2,48 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
-console.log("Hello from app/javascript/application.js")
+console.log("app/javascript/application.js is connected");
 
 // Get the modal
-
 document.addEventListener("turbo:load", () => {
-  let buttons = document.querySelectorAll(".index-cocktail-name");
   let myModal = document.querySelector(".modal");
-
+  let buttons = document.querySelectorAll(".index-cocktail-name");
   let modalName = document.querySelector(".show-cocktail-name");
-  // let modalIngredients = document.querySelector(".show-card-ingredients");
-  // let modalMethod = document.querySelector(".show-card-method");
+  let modalIngredients = document.querySelector(".show-card-ingredients");
+  let modalBottom = document.querySelector(".show-card-bottom");
 
   buttons.forEach((button) => {
-    // When the user clicks on <span> (x), close the modal
-    button.onclick = function() {
-      myModal.style.display = "none";
-    }
     // When the user clicks on the button, open the modal
     button.onclick = function() {
       myModal.style.display = "block";
-    }
+    };
 
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
       if (event.target == myModal) {
         myModal.style.display = "none";
       }
-    }
+    };
 
     button.addEventListener('click', (event) => {
-      myModal.id = button.id
-      console.log(button.getElementsByClassName("index-card-ingredients"));
-      modalName.innerHTML = button.getElementsByClassName("index-cocktail-name")[0].innerHTML
-      // modalIngredients.innerHTML = button.getElementsByClassName("index-card-ingredients")[0].innerHTML
-      // modalMethod.innerHTML = button.getElementsByClassName("show-card-method")[0].innerHTML
+      // Test console.logging:
+      // console.log(button.parentNode.parentNode.getElementsByClassName("index-card-bottom")[0].innerText)
+      // console.log(button.parentNode.getElementsByClassName("index-measurements")[0].outerHTML)
+      // console.log(button.parentNode.parentNode.getElementsByClassName("modal-bottom")[0])
+      // console.log(button.parentNode.parentNode.getElementsByClassName("modal-bottom")[0])
 
-      // Interpolation: (`myModal-${button.id}`)
-      // Get the button that opens the modal
-      var btn = document.getElementById(`myBtn-${button.id}`);
-      console.log(button.id);
-      // Get the <span> element that closes the modal
-      var span = document.getElementsByClassName("close")[0];
+      myModal.id = button.id;
+      modalName.innerHTML = button.innerHTML;
+      modalIngredients.innerHTML = button.parentNode.getElementsByClassName("index-measurements")[0].outerHTML;
+
+      // Below makes "index-measurements" removes styling from the HTML, making it visible:
+      modalIngredients.getElementsByClassName("index-measurements")[0].style = "none";
+      // modalIngredients.getElementsByClassName("index-measurements")[0])
+      // Need "modal-bottom" to have visibility, and remove height styling
+
+      // modalBottom Displays the Method and whether the drink contains alcohol or not:
+      modalBottom.innerHTML = button.parentNode.parentNode.getElementsByClassName("modal-bottom")[0].outerHTML;
+      modalBottom.parentNode.parentNode.getElementsByClassName("modal-bottom")[0].style = "none";
     })
   })
 })
