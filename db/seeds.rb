08b4599f5f0831b9f825aea2d6a218e9ad871b2d
adrienne robert vassy <adrienne.robertvassy@gmail.com>
2022-09-07 13:17:11 +0200

    measurements and ingredients worked out. However some weird measurements like Juice of 1 wedge remain

--- db/seeds.rb
+++ db/seeds.rb
@@ -122,20 +122,14 @@ def cocktails_dataset
   end
 
 
-  # cocktails = RestClient.get("www.thecocktaildb.com/api/json/v2/#{api_key}/search.php?s=bacardi")
-
-  # cocktails_array << JSON.parse(cocktails)["drinks"]
   cocktails_array = cocktails_array.flatten
 
-
-  # p cocktails_array.last['strDrink']
-  # p "************************"
-  # p cocktails_array.last["strDrink"]
+  
 
 
   cocktails_array.each do |c|
     var = Cocktail.create!(
-      name: c['strDrink'], #TypeError: no implicit conversion of String into Integer !!! :(
+      name: c['strDrink'],
       instructions: c['strInstructions'] || "Put everything in the glass",
       alcoholic: c['strAlcoholic'] || true,
       glass: c['strGlass'],
@@ -143,7 +137,7 @@ def cocktails_dataset
       IBA: c['strIBA'],
       tags: c['strTags']
     )
-    puts var.name
+    # puts var.name
   end
 
   puts "FINISHED creating #{Cocktail.count} cocktails"
@@ -153,80 +147,132 @@ def cocktails_dataset
 
   puts "************ Creating measurements and ingredients *************"
 
-    cocktails_array.each do |c|
+  cocktails_array.each do |c|
 
-      ingredient1 = c['strIngredient1']
-      ingredient2 = c['strIngredient2']
-      ingredient3 = c['strIngredient3']
-      ingredient4 = c['strIngredient4']
-      ingredient5 = c['strIngredient5']
-      ingredient6 = c['strIngredient6']
-      ingredient7 = c['strIngredient7']
-      ingredient8 = c['strIngredient8']
-      ingredient9 = c['strIngredient9']
-      ingredient10 = c['strIngredient10']
-      ingredient11 = c['strIngredient11']
-      ingredient12 = c['strIngredient12']
-      ingredient13 = c['strIngredient13']
-      ingredient14 = c['strIngredient14']
-      ingredient15 = c['strIngredient15']
+    # Get all ingredients of that cocktail
+    ingredient1 = c['strIngredient1']
+    ingredient2 = c['strIngredient2']
+    ingredient3 = c['strIngredient3']
+    ingredient4 = c['strIngredient4']
+    ingredient5 = c['strIngredient5']
+    ingredient6 = c['strIngredient6']
+    ingredient7 = c['strIngredient7']
+    ingredient8 = c['strIngredient8']
+    ingredient9 = c['strIngredient9']
+    ingredient10 = c['strIngredient10']
+    ingredient11 = c['strIngredient11']
+    ingredient12 = c['strIngredient12']
+    ingredient13 = c['strIngredient13']
+    ingredient14 = c['strIngredient14']
+    ingredient15 = c['strIngredient15']
 
-      ingredients_api[c['strDrink']] = [ingredient1, ingredient2, ingredient3, ingredient4, ingredient5, ingredient6, ingredient7,
-                                        ingredient8, ingredient9, ingredient10, ingredient11, ingredient12, ingredient13,
-                                        ingredient14, ingredient15].reject { |ing| ing == nil }
+    ingredients_api[c['strDrink']] = [ingredient1, ingredient2, ingredient3, ingredient4,
+      ingredient5, ingredient6, ingredient7, ingredient8, ingredient9, ingredient10, ingredient11,
+      ingredient12, ingredient13, ingredient14, ingredient15].reject { |ing| ing == nil }
 
-      uniq_ingredients_api = ingredients_api.values.flatten.uniq
+    # Clean up ingredients of that cocktail (capitalize, flatten array)
+    ingredients_api[c['strDrink']] = ingredients_api[c['strDrink']].map(&:titleize)
 
-      uniq_ingredients_api.each do |ing|
-        if Ingredient.where(name: ing).empty?
-          Ingredient.create!(name: ing)
-        end
+    uniq_ingredients_api = ingredients_api.values.flatten
+
+    # if new ingredient, create DB Ingredient instance
+    uniq_ingredients_api.each do |ing|
+      if Ingredient.where(name: ing).empty?
+        Ingredient.create!(name: ing)
       end
+    end
 
-      #  MEASUREMENTS
+    # start on measurements of that cocktail
+    # get all measurements of that cocktail
+    measure1 = c['strMeasure1']
+    measure2 = c['strMeasure2']
+    measure3 = c['strMeasure3']
+    measure4 = c['strMeasure4']
+    measure5 = c['strMeasure5']
+    measure6 = c['strMeasure6']
+    measure7 = c['strMeasure7']
+    measure8 = c['strMeasure8']
+    measure9 = c['strMeasure9']
+    measure10 = c['strMeasure10']
+    measure11 = c['strMeasure11']
+    measure12 = c['strMeasure12']
+    measure13 = c['strMeasure13']
+    measure14 = c['strMeasure14']
+    measure15 = c['strMeasure15']
 
-      measure1 = c['strMeasure1']
-      measure2 = c['strMeasure2']
-      measure3 = c['strMeasure3']
-      measure4 = c['strMeasure4']
-      measure5 = c['strMeasure5']
-      measure6 = c['strMeasure6']
-      measure7 = c['strMeasure7']
-      measure8 = c['strMeasure8']
-      measure9 = c['strMeasure9']
-      measure10 = c['strMeasure10']
-      measure11 = c['strMeasure11']
-      measure12 = c['strMeasure12']
-      measure13 = c['strMeasure13']
-      measure14 = c['strMeasure14']
-      measure15 = c['strMeasure15']
+    # put all measurements in one variable
+    # measurements_api[c['strDrink']]
+    measurements_of_c = [
+      measure1, measure2, measure3, measure4, measure5, measure6, measure7,
+      measure8, measure9, measure10, measure11, measure12, measure13,
+      measure14, measure15
+    ].reject { |m| m == nil || m == ""}
 
-      measurements_api[c['strDrink']] = [measure1, measure2, measure3, measure4, measure5, measure6, measure7,
-        measure8, measure9, measure10, measure11, measure12, measure13,
-        measure14, measure15].reject { |m| m == nil }
+    # Clean up measurements of that cocktail
 
-      # p measurements_api[c['strDrink']]
-      # p ingredients_api[c['strDrink']]
+    # 1/ IF ingr has NO measurement, add default values 1 unit
+    # by iterating through ingr and seeing if has relevant measurement
+
+    ingredients_api[c['strDrink']].each_with_index do |ingr, index|
+      # if !measurements_of_c[index].nil?
+      #   measurement[ingr] = measurements_of_c[index]
+      # else
+      #   measurement[ingr] = "1 unit"
+      # end
+      if measurements_of_c[index].nil?
+        measurements_of_c[index] = "1 unit"
+      end
+    end
+
+    # 2/ IF measurements is only a string/ only number
 
 
-      measurements_api[c['strDrink']].each_with_index do |measure, i|
-        cocktail_found = Cocktail.find_by(name: c['strDrink'])
-        ingredient = Ingredient.find_by(name: ingredients_api[c['strDrink']][i])
+    measurements_of_c.each_with_index do |measure, index|
+      cocktail_found = Cocktail.find_by(name: c['strDrink'])
+      ingredient = Ingredient.find_by(name: ingredients_api[c['strDrink']][index]) # not using  uniq_ingredients_api ??
+
+      if measure.strip.include?(" ") && measure.match?(/\s/)
         quantity = measure.split(" ")[0].to_i
         unit = measure.split(" ")[-1]
-
-        Measurement.create!(
-            cocktail: cocktail_found,
-            ingredient: ingredient,
-            quantity: quantity,
-            unit: unit
-          )
+      elsif !measure.strip.include?(" ") && measure.match?(/\s/)
+        quantity = 1
+        unit = measure
+      elsif !measure.strip.include?(" ") && !measure.match?(/\s/)
+        quantity = measure
+        unit = "unit"
       end
 
+      Measurement.create!(
+        cocktail: cocktail_found,
+        ingredient: ingredient,
+        quantity: quantity,
+        unit: unit
+      )
+
+      puts "ISSUE WHEERE FUCK***********************************"
+      p c['strDrink']
+      p measurements_of_c
+
     end
-    puts "Added new ingredients from cocktails. Total ingredients= #{Ingredient.count}"
-    puts "FINISHED creating #{Measurement.count} measurements"
+
+
+
+    # if uniq_ingredients_api.count != measurements_api[c['strDrink']].count
+    #   measurements_api[c['strDrink']] << "1 unit"
+    # end
+
+    #  MEASUREMENTS
+
+
+
+
+
+
+
   end
+  puts "Added new ingredients from cocktails. Total ingredients= #{Ingredient.count}"
+  puts "FINISHED creating #{Measurement.count} measurements"
+end
 
 cocktails_dataset()
 
