35ff737ab1bd34cf12ed7dbd71280d37925a0015
Lucas Luz <lucas@calazansluz.com>
2022-09-07 17:09:00 +0200

    logout and get started redirected to the propper place

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,7 @@ class ApplicationController < ActionController::Base
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :birth_date])
   end
+  def after_sign_out_path_for(resource_name)
+    root_path
+  end
 end
