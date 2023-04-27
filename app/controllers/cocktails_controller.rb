0651f8fc8152e8bd22519841195c025341115cf5
Lucas Luz <lucas@calazansluz.com>
2023-04-27 09:27:01 -0300

    cocktails controller

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -6,59 +6,36 @@ class CocktailsController < ApplicationController
   end
 
   def index
-
-    # .-. .-. .-. .-. .-. . .
-    # `-. |-  |-| |(  |   |-|
-    # `-' `-' ` ' ' ' `-' ' `
     @cocktails = Cocktail.all
     @ingredients = Ingredient.all
     @measurements = Measurement.all
     @user_ingredients_in_stock = Cabinet.where(user_id: current_user.id, in_stock: true)
-    @found_cocktails_id = []
-    @ready = []
-    @missing_one = []
-    @missing_two = []
     @saved_cocktails = SavedCocktail.where(user: current_user)
     @filtered_cocktails = []
 
-    # 1 - get input
-    create_ingredient_tags
-    # 3 - Search based on (in stock) cabinet || @user_ingredients_in_stock
+    create_ingredient_tags if params[:search_query].present? && current_user
     search_based_on_ingredient_tags
-    # 4 - Filter results to unique recurrence
-    @all_found_cocktails = Cocktail.where(id: @found_cocktails_id.uniq)
-    # 5 - Compare
+    filter_results_to_unique_recurrence
     compare_ingredients
-    # 6 - Filter cocktails (ready, -1, -2)
-    @filtered_cocktails = @ready, @missing_one, @missing_two
-    @filtered_cocktails.flatten!
-    @ready.flatten!
-    @missing_one.flatten!
-    @missing_two.flatten!
-    # raise
+    filter_cocktails
   end
 
-  def create_ingredient_tags
-    if params[:search_query].present? && current_user
-      query = Ingredient.where(name: params[:search_query].downcase)
-      if query.count >= 1
-        search_ingr = query.first
-      else
-        search_ingr = Ingredient.where('name LIKE ?', "%#{params[:search_query].downcase}%").first
-      end
+  private
 
-      # 2 - save to cabinet || @user_ingredients_in_stock
-      @idk = Cabinet.find_by(ingredient: search_ingr, user: current_user)
-      if @idk && @idk.in_stock == false
-        @idk.in_stock = true
-        @idk.save!
-      elsif @idk == nil && search_ingr != nil
-        Cabinet.create!(ingredient: search_ingr, user: current_user)
-      end
+  def create_ingredient_tags
+    query = Ingredient.where(name: params[:search_query].downcase)
+    if query.count >= 1
+      search_ingr = query.first
+    else
+      search_ingr = Ingredient.where('name LIKE ?', "%#{params[:search_query].downcase}%").first
     end
+    # 2 - save to cabinet || @user_ingredients_in_stock
+    @idk = Cabinet.find_or_create_by(ingredient: search_ingr, user: current_user)
+    @idk.update!(in_stock: true) if @idk.in_stock == false
   end
 
   def search_based_on_ingredient_tags
+    @found_cocktails_id = []
     @user_ingredients_in_stock.pluck(:ingredient_id).each do |ingredient|
       array = Measurement.where(ingredient_id: ingredient)
       array.each do |item|
@@ -68,19 +45,31 @@ class CocktailsController < ApplicationController
     end
   end
 
+  def filter_results_to_unique_recurrence
+    @all_found_cocktails = Cocktail.where(id: @found_cocktails_id)
+  end
+
   def compare_ingredients
-    @all_found_cocktails.pluck(:id).each do |cocktailid| #=> [id, id, id]
-      @result_cocktail_ingredients = Measurement.where(cocktail_id: cocktailid).pluck(:ingredient_id) #=> [id, id, id]
-      @result_cabinet_ingredients = @user_ingredients_in_stock.pluck(:ingredient_id) #=> [id, id, id]
-      @missing_ingredients = @result_cocktail_ingredients - @result_cabinet_ingredients
-      case @missing_ingredients.count
+    @ready, @missing_one, @missing_two = [], [], []
+    @all_found_cocktails.each do |cocktail|
+      result_cocktail_ingredients = cocktail.ingredients.pluck(:id)
+      result_cabinet_ingredients = @user_ingredients_in_stock.pluck(:ingredient_id)
+      missing_ingredients = result_cocktail_ingredients - result_cabinet_ingredients
+      case missing_ingredients.count
       when 0
-        @ready << Cocktail.where(id: cocktailid)
+        @ready << cocktail
       when 1
-        @missing_one << Cocktail.where(id: cocktailid)
+        @missing_one << cocktail
       when 2
-        @missing_two << Cocktail.where(id: cocktailid)
+        @missing_two << cocktail
       end
     end
+  end
+
+  def filter_cocktails
+    @filtered_cocktails = [@ready, @missing_one, @missing_two].flatten
+    @ready.flatten!
+    @missing_one.flatten!
+    @missing_two.flatten!
   end
 end
