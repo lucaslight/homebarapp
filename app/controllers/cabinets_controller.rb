42d1fc24b0903839ebdb5d132ff347d511d0f7af
Lucas Luz <lucas@calazansluz.com>
2023-04-29 20:22:02 -0300

    toggle and find or create by

--- app/controllers/cabinets_controller.rb
+++ app/controllers/cabinets_controller.rb
@@ -7,19 +7,13 @@ class CabinetsController < ApplicationController
 
   def remove_from_stock
     @cabinet = Cabinet.find(params[:id])
-    @cabinet.in_stock = !@cabinet.in_stock
-    @cabinet.save!
+    @cabinet.toggle!(:in_stock)
     redirect_back(fallback_location: root_path)
   end
 
   def add_to_stock
-    @item_cabinet = Cabinet.find_by(user_id: current_user.id, ingredient_id: params[:id])
-    if @item_cabinet && @item_cabinet.in_stock == false
-      @item_cabinet.in_stock = true
-      @item_cabinet.save!
-    elsif @item_cabinet == nil
-      Cabinet.create!(user_id: current_user.id, ingredient_id: params[:id])
-    end
+    @item_cabinet = Cabinet.find_or_create_by(user_id: current_user.id, ingredient_id: params[:id])
+    @item_cabinet.update(in_stock: true)
     redirect_back(fallback_location: root_path)
   end
 end
